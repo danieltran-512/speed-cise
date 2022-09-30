df9050d9a534142c6801484a6f4b718d73f57a2f
danieltran-512 <xkw1927@autuni.ac.nz>
2022-09-30 13:37:45 +1300

    search details

---
+++ speed-client/src/components/searchTable/tableColumns.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-sparse-arrays */
+const tablecolumns = [
+    {
+      Header: 'Title',
+      accessor: 'title'
+    }, {
+      Header: 'Authors',
+      accessor: 'authors'
+    }, {
+      Header: 'Source',
+      accessor: 'journal'
+    }, {
+      Header: 'Pub. Year',
+      accessor: 'publicationYear'
+    },{
+      Header: 'DOI',
+      accessor: 'doi'
+    },{
+      Header: 'Level of Evidence',
+      accessor: 'evidenceResults'
+    }
+  ]
+  module.exports = tablecolumns
