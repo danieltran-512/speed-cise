0374a4a7c2b9fb3f58dbcb67b1ca7f2b14ffb878
danieltran-512 <xkw1927@autuni.ac.nz>
2022-10-20 18:37:16 +1300

    search page

--- speed-client/cypress/e2e/landing.cy.js
+++ speed-client/cypress/e2e/landing.cy.js
@@ -1,22 +1,7 @@
 /// <reference types="cypress" />
 
-// Welcome to Cypress!
-//
-// This spec file contains a variety of sample tests
-// for a todo list app that are designed to demonstrate
-// the power of writing tests in Cypress.
-//
-// To learn more about how Cypress works and
-// what makes it such an awesome testing tool,
-// please read our getting started guide:
-// https://on.cypress.io/introduction-to-cypress
-
 describe('elements renders correctly', () => {
   beforeEach(() => {
-    // Cypress starts out with a blank slate for each test
-    // so we must tell it to visit our website with the `cy.visit()` command.
-    // Since we want to visit the same URL at the start of all our tests,
-    // we include it in our beforeEach function so that it runs before each test
     cy.visit('https://cise-speed-2022.herokuapp.com')
   })
 
@@ -53,10 +38,6 @@ describe('redirects to the search page if the user clicks on search', () => {
 
 describe('redirects to the search page if the user clicks on search', () => {
   beforeEach(() => {
-    // Cypress starts out with a blank slate for each test
-    // so we must tell it to visit our website with the `cy.visit()` command.
-    // Since we want to visit the same URL at the start of all our tests,
-    // we include it in our beforeEach function so that it runs before each test
     cy.visit('https://cise-speed-2022.herokuapp.com')
   })
   
